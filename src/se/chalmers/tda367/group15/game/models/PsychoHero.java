3781e33b60ee818bc7273bbaca0b6335ac2b5f14
Peter <peter-eliasson@hotmail.com>
2013-04-10 14:51:21 +0200

    Added inner thread to the PsychoHero class

--- src/se/chalmers/tda367/group15/game/models/PsychoHero.java
+++ src/se/chalmers/tda367/group15/game/models/PsychoHero.java
@@ -2,15 +2,32 @@ package se.chalmers.tda367.group15.game.models;
 
 /**
  * Class representing the entire game.
+ * 
  * @author Peter
- *
+ * 
  */
 public class PsychoHero implements PsychoHeroGame {
+	private Thread gameThread;
 	
 	/**
 	 * Starts the game
 	 */
-	public void start(){
-		
+	public void start() {
+		gameThread = new Thread(new GameThread());
+		gameThread.start();
+	}
+
+	/**
+	 * An inner class representing the thread used to run the game logic.
+	 * 
+	 * @author Peter
+	 * 
+	 */
+	private class GameThread implements Runnable {
+		@Override
+		public void run() {
+			//TODO: Do stuff here.
+		}
+
 	}
 }
