a770ac29ebecef9f14ed5fdb2534b5d267f8c698
sjrp <simon.jrp@gmail.com>
2013-04-13 13:32:49 +0200

    Gave player position and velocity

--- src/se/chalmers/tda367/group15/game/models/Player.java
+++ src/se/chalmers/tda367/group15/game/models/Player.java
@@ -5,6 +5,31 @@ package se.chalmers.tda367.group15.game.models;
  * @author simon
  *
  */
-public interface Player {
-	// This is a player FFW!
+public abstract class Player {
+	private float x, y, vel;
+	
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public float getVel() {
+		return vel;
+	}
+	
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public void setVel(float vel) {
+		this.vel = vel;
+	}
 }
