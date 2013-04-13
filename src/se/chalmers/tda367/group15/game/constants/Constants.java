c12fc40d1dec1d80f83c09afd73ac377bbdd1f7a
Peter <peter-eliasson@hotmail.com>
2013-04-13 11:23:13 +0200

    Added a Constants class for holding globally accessable constants

---
+++ src/se/chalmers/tda367/group15/game/constants/Constants.java
@@ -0,0 +1,15 @@
+package se.chalmers.tda367.group15.game.constants;
+
+/**
+ * A class for holding constant values that are global across the entire game.
+ * Such as the game name, preferred FPS and preferred size.
+ * 
+ * @author Peter
+ * 
+ */
+public class Constants {
+	/**
+	 * The name of the game.
+	 */
+	public static final String GAME_NAME = "Psycho Hero";
+}
