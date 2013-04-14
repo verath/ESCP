0ec97c4acd7dc4cd3add7502f434dbd3125a6628
Peter <peter-eliasson@hotmail.com>
2013-04-14 13:57:45 +0200

    Moving setup of gameContainer to the factory

--- src/se/chalmers/tda367/group15/game/SlickPsychoHero.java
+++ src/se/chalmers/tda367/group15/game/SlickPsychoHero.java
@@ -3,8 +3,6 @@ package se.chalmers.tda367.group15.game;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
-import se.chalmers.tda367.group15.game.constants.Constants;
-
 /**
  * Class representing a Psycho Hero game using the Slick2d framework.
  * 
@@ -13,8 +11,7 @@ import se.chalmers.tda367.group15.game.constants.Constants;
  */
 public class SlickPsychoHero implements PsychoHeroGame {
 
-    private final AppGameContainer gameContainer;
-
+	private final AppGameContainer gameContainer;
 
 	/**
 	 * Constructor for the PsychoHero game.
@@ -25,40 +22,27 @@ public class SlickPsychoHero implements PsychoHeroGame {
 	public SlickPsychoHero(final AppGameContainer gameContainer) {
 		this.gameContainer = gameContainer;
 	}
-	
-	 /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void start() {   
-            // If we are debugging, PRINT THEM INFOS!
-            gameContainer.setVerbose(Constants.DEBUG);
-            
-            // TODO: Allow for changing this resolution
-            try {
-                    gameContainer.setDisplayMode(1024, 768, false);
-            } catch (SlickException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                    return;
-            }
-            
-            try {
-                    gameContainer.start();
-            } catch (SlickException e) {
-                    // TODO handle exception?
-                    e.printStackTrace();
-                    return;
-            }
-    }
 
-    /**
-     * {@inheritDoc}
-     */
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void start() {
+		try {
+			gameContainer.start();
+		} catch (SlickException e) {
+			// TODO handle exception?
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void stop() {
 		gameContainer.exit();
 	}
 
-	
 }
