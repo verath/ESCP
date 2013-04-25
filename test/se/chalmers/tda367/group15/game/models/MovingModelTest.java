a3a9a6d63f0a3781174a5102a26a687ebaf741ff
Peter <peter-eliasson@hotmail.com>
2013-04-25 19:30:12 +0200

    Changed the MovingModelTest to not test unnecessary things. There really is no point in testing simple getters and setters for floats, those pretty much can not fail. Instead trying to focus on parts that have some logic value.

--- test/se/chalmers/tda367/group15/game/models/MovingModelTest.java
+++ test/se/chalmers/tda367/group15/game/models/MovingModelTest.java
@@ -1,35 +1,65 @@
 package se.chalmers.tda367.group15.game.models;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.awt.geom.Rectangle2D;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MovingModelTest {
-	
+
 	private static MovingModel testedModel;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		testedModel = new MovingModel() {};
+		testedModel = new MovingModel() {
+		};
 	}
-	
+
 	@Test
-	public void testSetX() {
-		testedModel.setX(10);
-		assertTrue(testedModel.getX() == 10);
+	public final void testGetBounds() {
+		int x = 10, y = 10, width = 10, height = 10;
+		testedModel.setX(x);
+		testedModel.setY(y);
+		testedModel.setWidth(width);
+		testedModel.setHeight(height);
+		Rectangle2D.Float b = new Rectangle2D.Float(x, y, width, height);
+		assertEquals(b, testedModel.getBounds());
 	}
-	
+
 	@Test
-	public void testSetY() {
-		testedModel.setY(10);
-		assertTrue(testedModel.getY() == 10);
+	public final void testTakeDamage() {
+		testedModel.setHealth(100);
+		testedModel.takeDamage(1);
+		assertTrue(testedModel.getHealth() == 99);
 	}
-	
+
 	@Test
-	public void testSetVelocity() {
-		testedModel.setVelocity(10);
-		assertTrue(testedModel.getVelocity() == 10);
+	public void testSetInvalidLocation() {
+		// Allow either gracefully handling of invalid locations, or exception
+		// as both methods make sense
+		try {
+			testedModel.setX(-200);
+			testedModel.setY(-200);
+			assertTrue(testedModel.getX() >= 0 && testedModel.getY() >= 0);
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+
+	@Test
+	public void testSetInvalidSize() {
+		// Allow either gracefully handling of invalid locations, or exception
+		// as both methods make sense
+		try {
+			testedModel.setHeight(-200);
+			testedModel.setWidth(-200);
+			assertTrue(testedModel.getX() >= 0 && testedModel.getY() >= 0);
+		} catch (IllegalArgumentException e) {
+
+		}
 	}
 
 }
