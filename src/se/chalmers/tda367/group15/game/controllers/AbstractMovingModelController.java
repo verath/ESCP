26a71770b657a856195f0b9da5a2e73560cec970
HeyHoProBro <carjan91@gmail.com>
2013-05-06 15:34:44 +0200

    Fix. Projectiles now check if they hit characters instead of characters checking if they collide with bullets.

--- src/se/chalmers/tda367/group15/game/controllers/AbstractMovingModelController.java
+++ src/se/chalmers/tda367/group15/game/controllers/AbstractMovingModelController.java
@@ -282,11 +282,11 @@ public abstract class AbstractMovingModelController {
 			Rectangle2D.Float bound2 = otherModel.getBounds();
 			if (bound1.intersects(bound2) && this.model != otherModel) {
 				dynamicCollsion = true;
-				if (model instanceof AbstractCharacterModel
-						&& otherModel instanceof AbstractProjectileModel) {
-					AbstractProjectileModel projectile = (AbstractProjectileModel) otherModel;
+				if (model instanceof AbstractProjectileModel
+						&& otherModel instanceof AbstractCharacterModel) {
+					AbstractProjectileModel projectile = (AbstractProjectileModel) model;
 					int damage = projectile.getDamage();
-					model.takeDamage(damage);
+					otherModel.takeDamage(damage);
 					projectile.setAlive(false);
 				}
 			}
