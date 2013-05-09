fef6ec1662dcd03b3f64a894533bf684c8c7885e
Erik Tholen <ltd180@gmail.com>
2013-05-09 17:18:51 +0200

    Enemies now die painfully

--- src/se/chalmers/tda367/group15/game/models/DummyEnemyModel.java
+++ src/se/chalmers/tda367/group15/game/models/DummyEnemyModel.java
@@ -1,5 +1,12 @@
 package se.chalmers.tda367.group15.game.models;
 
+import java.io.File;
+import java.util.Arrays;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 /**
  * Simple class for representing a dummy enemy. This enemy is only intended for
  * testing.
@@ -36,5 +43,21 @@ public class DummyEnemyModel extends AbstractCharacterModel {
 		setOffset(11);
 		setAlive(true);
 		setHealth(100);
+	}
+	
+	public Animation getDeathAnimation() {
+		File folder = new File("res/animation/enemy/coworker/death");
+		File[] files = folder.listFiles();
+		Arrays.sort(files);
+		Image[] image = new Image[files.length];
+
+		for (int i = 0; i < files.length; i++) {
+			try {
+				image[i] = new Image(files[i].getPath());
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+		}
+		return new Animation(image, 50, true);
 	}
 }
--- src/se/chalmers/tda367/group15/game/views/DummyEnemyView.java
+++ src/se/chalmers/tda367/group15/game/views/DummyEnemyView.java
@@ -14,19 +14,25 @@ import org.newdawn.slick.SlickException;
 
 import se.chalmers.tda367.group15.game.constants.Constants;
 import se.chalmers.tda367.group15.game.models.AbstractMovingModel;
+import se.chalmers.tda367.group15.game.models.DummyEnemyModel;
 
 /**
  * Class representing the view of a Dummy Enemy
+ * 
  * @author simon
- *
+ * 
  */
-public class DummyEnemyView implements View{
+public class DummyEnemyView implements View {
 
 	private final AbstractMovingModel model;
-	private final Animation animation;
-	
+	private Animation animation;
+	private Animation deathAnimation;
+	private boolean deathAnimationRunning = false;
+	private boolean deathAnimationInitiated = false;
+
 	/**
 	 * Creates a new view for the Dummy Enemy.
+	 * 
 	 * @param movingModel
 	 */
 	public DummyEnemyView(final AbstractMovingModel movingModel) {
@@ -53,34 +59,69 @@ public class DummyEnemyView implements View{
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		if(model.isAlive()) {
-			
+		if (model.isAlive()) {
+
 			if (!model.isMoving()) {
 				animation.stop();
 			} else if (animation.isStopped()) {
 				animation.start();
 			}
-			
+
 			float rotation = (float) model.getRotation();
-			
+
 			// rotates the current frame
 			g.rotate(model.getX() + model.getWidth() / 2,
 					model.getY() + model.getHeight() / 2, rotation);
-			animation.draw(model.getX() - model.getOffset(), model.getY() - model.getOffset());
+			animation.draw(model.getX() - model.getOffset(), model.getY()
+					- model.getOffset());
 			g.resetTransform();
-			if(Constants.SHOW_BOUNDS) {
+			if (Constants.SHOW_BOUNDS) {
 				g.setColor(Color.yellow);
 				Rectangle2D.Float e = model.getBounds();
 				g.drawRect((int) e.getX(), (int) e.getY(), (int) e.getWidth(),
 						(int) e.getHeight());
 			}
-			
 
 			Font f = g.getFont();
-			g.drawString("HP: " + model.getHealth(), model.getX(),
-					model.getY() - f.getLineHeight());
+			g.drawString("HP: " + model.getHealth(), model.getX(), model.getY()
+					- f.getLineHeight());
+		} else if (!model.isAlive()) {
+			if(!deathAnimationInitiated) {
+				DummyEnemyModel tmp = (DummyEnemyModel) model;
+				runDeathAnimation(tmp.getDeathAnimation());
+				deathAnimationInitiated = true;
+			}	
+			float rotation = (float) model.getRotation();
+			g.rotate(model.getX() + model.getWidth() / 2,
+					model.getY() + model.getHeight() / 2, rotation);
+
+			if (deathAnimationRunning) {
+				animation.draw(model.getX() - model.getOffset(), model.getY()
+						- model.getOffset());
+				g.resetTransform();
+				
+				deathAnimationRunning = !(deathAnimation.isStopped());
+						
+			} else {
+
+				Image dead = deathAnimation.getImage(deathAnimation
+						.getFrameCount() - 1);
+				dead.draw(model.getX() - model.getOffset(), model.getY()
+						- model.getOffset());
+
+				g.resetTransform();
+			}
 		}
-		
+
+	}
+
+	public void runDeathAnimation(Animation animation) {
+		if (!model.isAlive()) {
+			this.deathAnimationRunning = true;
+			this.deathAnimation = animation;
+			this.deathAnimation.setLooping(false);
+			this.animation = deathAnimation;
+		}
 	}
 
 }
