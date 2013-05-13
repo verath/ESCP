4d4621caf694965912c8042c259f3084bdbb4b5e
simonjrp <simon.jrp@gmail.com>
2013-05-13 12:47:50 +0200

    Moved everything but the logic in init method to AbstractRoomController

--- src/se/chalmers/tda367/group15/game/controllers/AbstractRoomController.java
+++ src/se/chalmers/tda367/group15/game/controllers/AbstractRoomController.java
@@ -2,15 +2,23 @@ package se.chalmers.tda367.group15.game.controllers;
 
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Float;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+import org.newdawn.slick.util.pathfinding.PathFindingContext;
 import org.newdawn.slick.util.pathfinding.TileBasedMap;
 
+import se.chalmers.tda367.group15.game.constants.Constants;
 import se.chalmers.tda367.group15.game.models.AbstractMovingModel;
+import se.chalmers.tda367.group15.game.models.DummyEnemyModel;
 
 /**
  * Abstract class for representing a room containing a tiled map and controllers
@@ -22,9 +30,32 @@ import se.chalmers.tda367.group15.game.models.AbstractMovingModel;
 public abstract class AbstractRoomController implements TileBasedMap {
 
 	/**
-	 * A reference to the game's gameController
+	 * A reference to the game's gameController.
 	 */
 	private GameController gameController;
+
+	/**
+	 * The tiled map to be used in the room
+	 */
+	private TiledMap map;
+
+	/**
+	 * List containing the static bounds represented by rectangles.
+	 */
+	private List<Rectangle2D.Float> staticBounds;
+
+	/**
+	 * Map containing models and it's corresponding collision rectangles.
+	 */
+	private Map<AbstractMovingModel, Rectangle2D.Float> dynamicBounds;
+
+	/**
+	 * List containing controllers for all moving models in the room.
+	 */
+	private List<AbstractMovingModelController> movingModelControllers;
+
+	// List used in update method to add new controllers
+	private List<AbstractMovingModelController> quedControllers = new ArrayList<AbstractMovingModelController>();
 
 	/**
 	 * Creates a new AbstractMovingModelController.
@@ -33,6 +64,250 @@ public abstract class AbstractRoomController implements TileBasedMap {
 	 */
 	protected AbstractRoomController(GameController gameController) {
 		this.setGameController(gameController);
+		staticBounds = new ArrayList<Rectangle2D.Float>();
+		dynamicBounds = new HashMap<AbstractMovingModel, Rectangle2D.Float>();
+		movingModelControllers = new ArrayList<AbstractMovingModelController>();
+	}
+
+	/**
+	 * Method for adding a moving model to this room.
+	 * 
+	 * @param model
+	 *            The model to be added
+	 */
+	public void addMovingModel(DummyEnemyModel model) {
+		movingModelControllers.add(new DummyEnemyController(model, this,
+				gameController));
+		dynamicBounds.put(model, model.getBounds());
+	}
+
+	// TODO add more describing javadoc.. Calle :)?
+	/**
+	 * Method for adding a moving model to this room.
+	 * 
+	 * @param model
+	 *            The model to be added
+	 * @param x1
+	 *            minimum x
+	 * @param x2
+	 *            maximum x
+	 * @param y1
+	 *            minimum y
+	 * @param y2
+	 *            maximum y
+	 */
+	public void addMovingModel(DummyEnemyModel model, int x1, int x2, int y1,
+			int y2) {
+		movingModelControllers.add(new DummyEnemyController(model, this,
+				gameController, x1, x2, y1, y2));
+		dynamicBounds.put(model, model.getBounds());
+	}
+
+	/**
+	 * Method for adding a projectile to the room.
+	 * 
+	 * @param projectile
+	 *            The projectile to be added
+	 */
+	public void addProjectile(AbstractMovingModel projectile) {
+		quedControllers.add(new ProjectileController(getGameController(),
+				projectile));
+		dynamicBounds.put(projectile, projectile.getBounds());
+	}
+
+	/**
+	 * Method for adding a swing infront of the hero
+	 * 
+	 * @param swing
+	 *            The swing to be added
+	 */
+	public void addSwing(AbstractMovingModel swing) {
+		quedControllers
+				.add(new MeleeSwingController(getGameController(), swing));
+		dynamicBounds.put(swing, swing.getBounds());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean blocked(PathFindingContext context, int tx, int ty) {
+		// TODO How to use PathFindingContext?
+		try {
+			int tileID = map.getTileId(tx, ty, 1);
+			String property = map.getTileProperty(tileID, "blocked", "false");
+			if (property.equals("false")) {
+				return false;
+			}
+			return true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return true;
+		}
+	}
+
+	/**
+	 * Method for getting controllers for all moving models in the room.
+	 * 
+	 * @return List of all moving model controllers.
+	 */
+	public List<AbstractMovingModelController> getControllers() {
+		return movingModelControllers;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public float getCost(PathFindingContext context, int tx, int ty) {
+		return 1;
+	}
+
+	/**
+	 * Method for getting a map of all dynamic collision bounds.
+	 * 
+	 * @return A map containing models and it's corresponding collision bounds.
+	 */
+	public Map<AbstractMovingModel, Rectangle2D.Float> getDynamicBounds() {
+		// update bounds
+		Iterator<AbstractMovingModel> it = dynamicBounds.keySet().iterator();
+
+		while (it.hasNext()) {
+			AbstractMovingModel model = (AbstractMovingModel) it.next();
+			if (model.isAlive()) {
+				dynamicBounds.put(model, model.getBounds());
+			} else {
+				it.remove();
+			}
+		}
+		return dynamicBounds;
+	}
+
+	/**
+	 * Getter for the gameController associated with this controller.
+	 * 
+	 * @return the game controller
+	 */
+	protected GameController getGameController() {
+		return gameController;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getHeightInTiles() {
+		return map.getHeight(); // *map.getTileHeight(); if using pixels instead
+	}
+
+	/**
+	 * Method for getting the tiled map of the room.
+	 * 
+	 * @return A tiled map
+	 */
+	public TiledMap getMap() {
+		return map;
+	}
+
+	/**
+	 * Method for getting the static collision bounds of the room.
+	 * 
+	 * @return A list of static collision bounds.
+	 */
+	public List<Rectangle2D.Float> getStaticBounds() {
+		return staticBounds;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getWidthInTiles() {
+		return map.getWidth(); // *map.getTileWidth(); if using pixels instead
+	}
+
+	/**
+	 * Method for initializing the room.
+	 * 
+	 * @param container
+	 *            The container holding this game.
+	 * @throws SlickException
+	 *             Throw to indicate an internal error.
+	 */
+	public abstract void init(GameContainer container) throws SlickException;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void pathFinderVisited(int x, int y) {
+		// This is for debugging new heuristics.
+	}
+
+	/**
+	 * Method for rendering all model views in the room.
+	 * 
+	 * @param container
+	 *            The container holding this game.
+	 * @param g
+	 *            The graphics context that can be used to render. However,
+	 *            normal rendering routines can also be used.
+	 * @throws SlickException
+	 *             Throw to indicate an internal error
+	 */
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		// render map
+		map.render(0, 0);
+
+		// tell enemy controllers to render all enemy views
+		for (AbstractMovingModelController controller : movingModelControllers) {
+			controller.render(container, g);
+		}
+
+		if (Constants.SHOW_BOUNDS) {
+			g.setColor(Color.red);
+			for (Rectangle2D.Float e : staticBounds) {
+				g.drawRect((int) e.getX(), (int) e.getY(), (int) e.getWidth(),
+						(int) e.getHeight());
+			}
+		}
+	}
+
+	/**
+	 * Setter for the gameController to be associated with this controller.
+	 * 
+	 * @param gameController
+	 */
+	protected void setGameController(GameController gameController) {
+		this.gameController = gameController;
+	}
+
+	/**
+	 * Method for setting the map to be used in the room.
+	 * 
+	 * @param mapPath
+	 *            Path in filesystem to the tmx file representing a tiled map.
+	 */
+	public void setMap(String mapPath) {
+		try {
+			this.map = new TiledMap(mapPath);
+
+			// add static collision bounds
+			for (int i = 0; i < map.getWidth(); i++) {
+				for (int j = 0; j < map.getHeight(); j++) {
+					int tileID = map.getTileId(i, j, 1);
+					String property = map.getTileProperty(tileID, "blocked",
+							"false");
+					if (property.equals("true")) {
+						staticBounds.add(new Rectangle2D.Float(i * 32, j * 32,
+								32, 32));
+					}
+				}
+			}
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -52,73 +327,30 @@ public abstract class AbstractRoomController implements TileBasedMap {
 	 * @throws SlickException
 	 *             Throw to indicate an internal error.
 	 */
-	public abstract void update(GameContainer container, int delta,
+	public void update(GameContainer container, int delta,
 			List<Float> staticBounds,
 			Map<AbstractMovingModel, Float> dynamicBounds)
-			throws SlickException;
+			throws SlickException {
+		// tell enemy controllers to move
 
-	/**
-	 * Method for rendering all model views in the room.
-	 * 
-	 * @param container
-	 *            The container holding this game.
-	 * @param g
-	 *            The graphics context that can be used to render. However,
-	 *            normal rendering routines can also be used.
-	 * @throws SlickException
-	 *             Throw to indicate an internal error
-	 */
-	public abstract void render(GameContainer container, Graphics g)
-			throws SlickException;
+		Iterator it1 = movingModelControllers.iterator();
+		while (it1.hasNext()) {
+			AbstractMovingModelController controller = (AbstractMovingModelController) it1
+					.next();
+			if (controller.getModel().isAlive()) {
+				controller
+						.update(container, delta, staticBounds, dynamicBounds);
+			}
+		}
 
-	/**
-	 * Method for initializing the room.
-	 * 
-	 * @param container
-	 *            The container holding this game.
-	 * @throws SlickException
-	 *             Throw to indicate an internal error.
-	 */
-	public abstract void init(GameContainer container) throws SlickException;
-
-	public abstract List<Rectangle2D.Float> getStaticBounds();
-
-	public abstract List<AbstractMovingModelController> getControllers();
-
-	public abstract Map<AbstractMovingModel, Rectangle2D.Float> getDynamicBounds();
-
-	/**
-	 * Getter for the gameController associated with this controller.
-	 * 
-	 * @return
-	 */
-	protected GameController getGameController() {
-		return gameController;
+		// add new qued controllers to the movingModelControllers list
+		Iterator it2 = quedControllers.iterator();
+		while (it2.hasNext()) {
+			AbstractMovingModelController controller = (AbstractMovingModelController) it2
+					.next();
+			movingModelControllers.add(controller);
+		}
+		quedControllers.clear();
 	}
-
-	/**
-	 * Setter for the gameController to be associated with this controller.
-	 * 
-	 * @param gameController
-	 */
-	protected void setGameController(GameController gameController) {
-		this.gameController = gameController;
-	}
-
-	/**
-	 * Method for adding a projectile to the room.
-	 * 
-	 * @param projectile
-	 *            The projectile to be added
-	 */
-	public abstract void addProjectile(AbstractMovingModel projectile);
-
-	/**
-	 * Method for adding a swing infront of the hero
-	 * 
-	 * @param swing
-	 *            The swing to be added
-	 */
-	public abstract void addSwing(AbstractMovingModel swing);
 
 }
