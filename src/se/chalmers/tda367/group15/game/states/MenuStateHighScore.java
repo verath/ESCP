7a09453ddb7654e1e74257a0f4f53cb8a5b5e453
Peter <peter-eliasson@hotmail.com>
2013-05-19 15:35:57 +0200

    Changed to monospaced font for high score, to align better

--- src/se/chalmers/tda367/group15/game/states/MenuStateHighScore.java
+++ src/se/chalmers/tda367/group15/game/states/MenuStateHighScore.java
@@ -1,12 +1,13 @@
 package se.chalmers.tda367.group15.game.states;
 
+import java.awt.*;
 import java.util.List;
+
+import org.newdawn.slick.*;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
-
 import se.chalmers.tda367.group15.game.database.DatabaseScore;
 import se.chalmers.tda367.group15.game.database.GameDatabase;
 import se.chalmers.tda367.group15.game.menu.Button;
@@ -27,14 +28,20 @@ public class MenuStateHighScore extends AbstractMenuBasedState {
 	private static final int HIGH_SCORE_LIST_Y = 170;
 
 	/**
+	 * The font used in this state to draw string
+	 */
+	private final TrueTypeFont textFont = new TrueTypeFont(new java.awt.Font(
+			"Monospaced", java.awt.Font.BOLD, 24), true);
+
+	/**
 	 * Horizontal spacing between entries
 	 */
-	private static final int HIGH_SCORE_LIST_SPACING = 30;
+	private static final int HIGH_SCORE_LIST_SPACING = 32;
 
 	/**
 	 * The spacing vertically between the name and the score.
 	 */
-	private static final int HIGH_SCORE_LIST_WIDTH = 200;
+	private static final int HIGH_SCORE_LIST_WIDTH = 300;
 
 	/**
 	 * The number of high score entries to show
@@ -81,14 +88,14 @@ public class MenuStateHighScore extends AbstractMenuBasedState {
 				e.printStackTrace();
 			}
 		}
-        updateHighScoreList();
+		updateHighScoreList();
 	}
 
-    @Override
-    protected void initMenuItems() {
-    }
+	@Override
+	protected void initMenuItems() {
+	}
 
-    @Override
+	@Override
 	public void render(Graphics g) {
 		super.render(g);
 
@@ -112,14 +119,16 @@ public class MenuStateHighScore extends AbstractMenuBasedState {
 				}
 
 				// Add ranking to the name, easier to align than if it were a
-				// separate text string
-				name = Integer.toString(i + 1) + ". " + name;
+				// separate text string. Also pad < 10 with a space
+				if ((i + 1) < 10) {
+					name = " " + Integer.toString(i + 1) + ". " + name;
+				} else {
+					name = Integer.toString(i + 1) + ". " + name;
+				}
 
-				g.getFont().drawString(HIGH_SCORE_LIST_X,
-						HIGH_SCORE_LIST_Y + HIGH_SCORE_LIST_SPACING * i, name,
-						Color.white);
-				g.getFont().drawString(
-						HIGH_SCORE_LIST_X + HIGH_SCORE_LIST_WIDTH,
+				textFont.drawString(HIGH_SCORE_LIST_X, HIGH_SCORE_LIST_Y
+						+ HIGH_SCORE_LIST_SPACING * i, name, Color.white);
+				textFont.drawString(HIGH_SCORE_LIST_X + HIGH_SCORE_LIST_WIDTH,
 						HIGH_SCORE_LIST_Y + HIGH_SCORE_LIST_SPACING * i, score,
 						Color.white);
 			}
@@ -134,7 +143,7 @@ public class MenuStateHighScore extends AbstractMenuBasedState {
 
 		// Button for returning to main menu.
 		Button returnButton = new Button(container, backImage, backImageMO,
-				Constants.MENU_UPPER_X+50, Constants.MENU_UPPER_Y) {
+				Constants.MENU_UPPER_X + 50, Constants.MENU_UPPER_Y) {
 			@Override
 			public void performAction() {
 				game.enterState(Constants.GAME_STATE_MENU_MAIN);
@@ -158,19 +167,19 @@ public class MenuStateHighScore extends AbstractMenuBasedState {
 		updateHighScoreList();
 	}
 
-    /**
-     * Forces a refresh of the displayed high score list
-     */
-    public void updateHighScoreList() {
-        GameDatabase db;
-        try {
-            db = new GameDatabase();
-            highScores = db.getHighscores(NUM_HIGH_SCORE_ENTRIES);
-        } catch (ClassNotFoundException e) {
-            if (Constants.DEBUG) {
-                e.printStackTrace();
-            }
-        }
-    }
+	/**
+	 * Forces a refresh of the displayed high score list
+	 */
+	public void updateHighScoreList() {
+		GameDatabase db;
+		try {
+			db = new GameDatabase();
+			highScores = db.getHighscores(NUM_HIGH_SCORE_ENTRIES);
+		} catch (ClassNotFoundException e) {
+			if (Constants.DEBUG) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
