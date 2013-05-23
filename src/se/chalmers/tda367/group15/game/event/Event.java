d4f12c035708a1ae4b406afd45599579e7c5b7a8
Peter <peter-eliasson@hotmail.com>
2013-05-23 08:51:02 +0200

    Added class comment

--- src/se/chalmers/tda367/group15/game/event/Event.java
+++ src/se/chalmers/tda367/group15/game/event/Event.java
@@ -1,4 +1,9 @@
 package se.chalmers.tda367.group15.game.event;
 
+/**
+ * The abstract base class that all events using the event system should extend.
+ *
+ * @author Peter
+ */
 public abstract class Event {
 }
